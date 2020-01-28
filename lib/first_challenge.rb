a48d737cef23f2b52c05d4ef67153a04731244c8
Jeremy Bourne <jbourne25@yahoo.com>
2020-01-28 05:13:45 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,10 +1,15 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {
+      :montague => {
        :patriarch => {
          :name => "Lord Montague",
          :age => "53"
-       }
-     }
-   }
+      },
+      :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
+      },
+        
+     
+   
 end
